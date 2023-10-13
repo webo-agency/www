25ab324b8ed54d4a9f9a6904eddfb673c99a4b9c
Kamil <k.medrala99@gmail.com>
2023-10-13 11:34:06 +0200

    navbar fixes

--- components/lang-switcher.vue
+++ components/lang-switcher.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative group w-fit text-center">
-    <div class="flex gap-2.5 items-center py-3 desktop:p-1 cursor-pointer">
+    <div class="flex gap-2.5 items-center py-3 desktop:p-3 cursor-pointer">
       <span
         class="shrink-0 block text-inherit text-lg font-semibold transition duration-300 group-hover:text-green-main"
         >{{ currLang }}</span
@@ -10,7 +10,7 @@
     </div>
     
     <div
-      class="desktop:absolute z-10 desktop:left-1/2 -translate-y-5 group-hover:translate-y-0 desktop:translate-y-0 desktop:-translate-x-1/2 desktop:top-0 desktop:pt-[84px] opacity-0 pointer-events-none group-hover:pointer-events-auto group-hover:opacity-100 hover:opacity-100 hover:pointer-events-auto transition duration-300"
+      class="desktop:absolute z-10 desktop:left-1/2 -translate-y-5 group-hover:translate-y-0 desktop:translate-y-0 desktop:-translate-x-1/2 desktop:top-0 desktop:pt-[96px] opacity-0 pointer-events-none group-hover:pointer-events-auto group-hover:opacity-100 hover:opacity-100 hover:pointer-events-auto transition duration-300"
     >
     <div class="relative filter blur-0 brightness-100 contrast-100 grayscale-0 hue-rotate-0 saturate-100 invert-0 sepia-0 desktop:drop-shadow-[0_20px_25px_rgba(66,66,66,0.2)]">
         <div class="hidden desktop:block absolute top-[-11px] left-1/2 -translate-x-1/2 w-6 h-6 bg-white rotate-45 rounded-[3px] transition-all duration-200 translate-y-4 group-hover:translate-y-0"></div>
