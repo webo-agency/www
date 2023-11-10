5ef1f25a9ae5271bae32e4e60454eaf1c4d144e7
Kamil <k.medrala99@gmail.com>
2023-11-10 16:07:18 +0100

    clutch review iframe

---
+++ components/rating-clutch.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class=" w-[204px] overflow-hidden">
+        <div class="clutch-widget" data-url="https://widget.clutch.co" data-widget-type="2" data-height="45"
+            data-nofollow="true" data-expandifr="true" data-scale="100" data-clutchcompany-id="1892969"></div>
+    </div>
+</template>
+<script setup>
+useHead({
+    script: [
+        { type: "text/javascript", src: 'https://widget.clutch.co/static/js/widget.js', async: true }
+    ]
+})
+</script>
