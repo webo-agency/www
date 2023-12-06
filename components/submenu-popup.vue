af84e277d77e12f8b549cff9a673e4cf10944713
Kamil <k.medrala99@gmail.com>
2023-12-06 17:00:00 +0100

    menu fix

--- components/submenu-popup.vue
+++ components/submenu-popup.vue
@@ -47,7 +47,7 @@
           </div>
           <ul
             v-if="data && data.links"
-            class="flex flex-wrap items-start justify-between w-2/3"
+            class="flex flex-wrap items-start justify-between w-2/3 desktop-wide:w-3/5"
           >
             <li
               v-for="item in data.links"
