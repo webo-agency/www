2038318ee141e1976f23439b8c1ecc2f582c4a1e
Tomek <TK-54-4B>
2024-01-24 17:26:40 +0100

    test

--- components/content/offer-tile-md.vue
+++ components/content/offer-tile-md.vue
@@ -11,7 +11,7 @@
       class="desktop-delay h-[460px]"
       :class="[
         { '!h-[160px] desktop:!h-[270px]': isSmallTile },
-        { '!h-[370px]': !iconBg },
+        { '!h-[370px]': !technologies },
       ]"
       :delay="delay * 100"
     >
