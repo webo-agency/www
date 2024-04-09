de29398e0d72f2eac9a0d286e5b24e95521ce5ca
Tomek <TK-54-4B>
2024-04-09 20:45:26 +0200

    page about us, added section 4

---
+++ components/content/block-technologies-grid-md.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div
+      v-if="$slots.title"
+      class="title max-w-fit mb-5 text-xl desktop:text-2xl font-semibold"
+    >
+      <ContentSlot :use="$slots.title" />
+    </div>
+
+    <div
+      :class="[
+        tilesTight
+          ? 'grid grid-cols-3 tablet:grid-cols-6 grid-rows-4 tablet:grid-rows-2'
+          : 'grid grid-cols-2 grid-rows-2 gap-5',
+      ]"
+    >
+      <slot> </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BlockTechnologiesGridMdc",
+  props: {
+    tilesTight: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ components/content/technology-tile-md.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="">
+    <EffectAppearMdc
+      class="desktop-delay"
+      :class="[tilesSmall ? 'h-[125px] tablet:h-[145px]' : 'h-[135px]']"
+      :delay="delay * 100"
+    >
+      <div
+        class="group h-full flex flex-col items-center justify-center text-gray-darker bg-green-light transition duration-200"
+      >
+        <nuxt-picture
+          v-if="icon"
+          class="shrink-0 w-20 mb-2"
+          :class="[tilesSmall ? 'w-[60px] tablet:w-[80px]' : 'w-[100px]']"
+          :src="icon"
+        ></nuxt-picture>
+        <div v-if="$slots.title" class="title max-w-fit font-semibold">
+          <ContentSlot :use="$slots.title" />
+        </div>
+      </div>
+    </EffectAppearMdc>
+  </div>
+</template>
+
+<script>
+import EffectAppearMdc from "./effect-appear-md.vue";
+import CustomLink from "../custom-link.vue";
+export default {
+  name: "TechnologyTileMdc",
+  props: {
+    link: String,
+    icon: String,
+    technologies: String,
+    delay: {
+      type: Number,
+      default: 0,
+    },
+    tilesSmall: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  components: { EffectAppearMdc, CustomLink },
+};
+</script>
+
+<style></style>
