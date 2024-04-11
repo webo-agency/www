72a5d13bf95641e93ad14ceac638fcb7aecf2af8
Tomek <TK-54-4B>
2024-04-11 20:48:50 +0200

    changed z-index of scroll btn and messenger widget

--- components/button-scroll-up.vue
+++ components/button-scroll-up.vue
@@ -1,61 +1,70 @@
 <template>
-    <div class="w-full sticky z-40 bottom-0 px-5 tablet:px-10 desktop:px-20 pointer-events-none ">
-        <div class=" flex justify-end max-w-screen-desktop-wide mx-auto pointer-events-none h-0 items-end overflow-visible  transition duration-200"
-        :class="{'translate-y-[100px] opacity-0' :isHidden}">
-            <button class="appearance-none flex items-center justify-center bg-green-main rounded-full w-[60px] h-[60px] hover:bg-green-mainHover bottom-0 mb-[90px] shadow-green-mainHover/40 shadow-lg transition duration-300 pointer-events-auto"
-             @click="scrollToTop()"
-             >
-                <svg width="19" height="25" viewBox="0 0 19 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M0.916503 8.58363L2.35919 10.0263L8.47992 3.90559L8.47992 25L10.5203 25L10.5203 3.90578L16.6408 10.0263L18.0835 8.58363L9.50001 -3.75197e-07L0.916503 8.58363Z" fill="#FAFAFA"/>
-                </svg>
-            </button>
-        </div>
+  <div
+    class="w-full sticky z-30 bottom-0 px-5 tablet:px-10 desktop:px-20 pointer-events-none"
+  >
+    <div
+      class="flex justify-end max-w-screen-desktop-wide mx-auto pointer-events-none h-0 items-end overflow-visible transition duration-200"
+      :class="{ 'translate-y-[100px] opacity-0': isHidden }"
+    >
+      <button
+        class="appearance-none flex items-center justify-center bg-green-main rounded-full w-[60px] h-[60px] hover:bg-green-mainHover bottom-0 mb-[90px] shadow-green-mainHover/40 shadow-lg transition duration-300 pointer-events-auto"
+        @click="scrollToTop()"
+      >
+        <svg
+          width="19"
+          height="25"
+          viewBox="0 0 19 25"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M0.916503 8.58363L2.35919 10.0263L8.47992 3.90559L8.47992 25L10.5203 25L10.5203 3.90578L16.6408 10.0263L18.0835 8.58363L9.50001 -3.75197e-07L0.916503 8.58363Z"
+            fill="#FAFAFA"
+          />
+        </svg>
+      </button>
     </div>
+  </div>
 </template>
 
 <script>
-import { useWindowScroll, useWindowSize } from '@vueuse/core'
+import { useWindowScroll, useWindowSize } from "@vueuse/core";
 
-export default{
-    setup(){
-        const { height } = useWindowSize()        
-        const { y } = useWindowScroll()
-        return {y,height}
+export default {
+  setup() {
+    const { height } = useWindowSize();
+    const { y } = useWindowScroll();
+    return { y, height };
+  },
+  data() {
+    return {
+      isHidden: true,
+    };
+  },
+  watch: {
+    y(value, height) {
+      this.checkScrollOffset();
     },
-    data(){
-        return{
-            isHidden: true
-        }
+  },
+  mounted() {
+    this.checkScrollOffset();
+  },
+  methods: {
+    checkScrollOffset() {
+      const windowHeight = this.height;
+      if (windowHeight) {
+        this.isHidden = this.y < windowHeight;
+      }
     },
-    watch:{
-        y(value,height){
-            this.checkScrollOffset()
-        }
-    },
-    mounted(){
-        this.checkScrollOffset()
-    },
-    methods:{
-        checkScrollOffset(){
-            const windowHeight = this.height
-            if (windowHeight) {
-                this.isHidden =  this.y < windowHeight 
-            }
-        },
-        
-        scrollToTop() {
-              window.scrollTo({
-                top: 0,
-                behavior: "smooth",
-              });
-            }
-    }
-}
 
-
-
+    scrollToTop() {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- components/fb-chat.client.vue
+++ components/fb-chat.client.vue
@@ -1,97 +1,92 @@
 <template>
-  <div class="w-full sticky z-40 bottom-0 px-5 tablet:px-10 desktop:px-20">
-        <div class=" flex justify-end max-w-screen-desktop-wide mx-auto h-0">
-          <div id="fb-root"></div>
-          <div id="fb-customer-chat" class="fb-customerchat">
-        </div>
-        </div>
+  <div class="w-full sticky z-30 bottom-0 px-5 tablet:px-10 desktop:px-20">
+    <div class="flex justify-end max-w-screen-desktop-wide mx-auto h-0">
+      <div id="fb-root"></div>
+      <div id="fb-customer-chat" class="fb-customerchat"></div>
     </div>
+  </div>
 </template>
 <script setup>
-
 const props = defineProps({
-    id: Number,
-    locale: String
-})
+  id: Number,
+  locale: String,
+});
 
-function addFacebookSDK(){
-    const d = document;
-    const s = "script";
-    const id = "facebook-jssdk";
+function addFacebookSDK() {
+  const d = document;
+  const s = "script";
+  const id = "facebook-jssdk";
 
-    var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = `https://connect.facebook.net/${props.locale}/sdk/xfbml.customerchat.js`;
-        js.setAttribute('defer', 'defer');
-        js.setAttribute('async', 'async');
-        fjs.parentNode.insertBefore(js, fjs);
+  var js,
+    fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = `https://connect.facebook.net/${props.locale}/sdk/xfbml.customerchat.js`;
+  js.setAttribute("defer", "defer");
+  js.setAttribute("async", "async");
+  fjs.parentNode.insertBefore(js, fjs);
 }
 
 function initFacebookChat() {
-      window.fbAsyncInit = function () {
-        const chatbox = document.getElementById('fb-customer-chat');
-        if (chatbox) {
-          chatbox.setAttribute("page_id", props.id);
-          chatbox.setAttribute("attribution", "biz_inbox");
-          FB.init({
-            xfbml: true,
-            version: 'v18.0',
-          });
-        }
-      };
-      addFacebookSDK();
+  window.fbAsyncInit = function () {
+    const chatbox = document.getElementById("fb-customer-chat");
+    if (chatbox) {
+      chatbox.setAttribute("page_id", props.id);
+      chatbox.setAttribute("attribution", "biz_inbox");
+      FB.init({
+        xfbml: true,
+        version: "v18.0",
+      });
     }
+  };
+  addFacebookSDK();
+}
 
-onMounted(()=>{
-    initFacebookChat()
-})
+onMounted(() => {
+  initFacebookChat();
+});
 </script>
 
 <style>
-#fb-root{
-  @apply !relative !bottom-0 !z-40
+#fb-root {
+  @apply !relative !bottom-0 !z-40;
 }
 
-.fb_dialog{
-  @apply !top-0 !relative !h-0 !overflow-visible !p-0
+.fb_dialog {
+  @apply !top-0 !relative !h-0 !overflow-visible !p-0;
 }
 
-.fb_dialog_content iframe{
-  @apply !absolute !bottom-4 !right-0 !mx-0
+.fb_dialog_content iframe {
+  @apply !absolute !bottom-4 !right-0 !mx-0;
 }
 
-.fb_dialog_content > div{
-  @apply !absolute !bottom-4 !right-0 !mx-0
+.fb_dialog_content > div {
+  @apply !absolute !bottom-4 !right-0 !mx-0;
 }
 
-
-.fb_iframe_widget{
-  @apply !overflow-visible !bottom-0 !w-full
+.fb_iframe_widget {
+  @apply !overflow-visible !bottom-0 !w-full;
 }
 
-.fb_iframe_widget > span{
-  @apply !w-full
+.fb_iframe_widget > span {
+  @apply !w-full;
 }
 
-.fb_new_ui_mobile_overlay_active .fb_iframe_widget iframe{
-  @apply tablet:!fixed
+.fb_new_ui_mobile_overlay_active .fb_iframe_widget iframe {
+  @apply tablet:!fixed;
 }
 
-.fb_iframe_widget iframe{
-  @apply !fixed tablet:!absolute
+.fb_iframe_widget iframe {
+  @apply !fixed tablet:!absolute;
 }
 
-.fb_iframe_widget iframe.fb_customer_chat_bounce_in_v2{
-  @apply !max-h-[calc(100vh_-_84px)]
+.fb_iframe_widget iframe.fb_customer_chat_bounce_in_v2 {
+  @apply !max-h-[calc(100vh_-_84px)];
 }
 
-.fb-customerchat > div{
+.fb-customerchat > div {
   transform: translateZ(10px) translateX(24px) translateY(8px);
-  @apply absolute
+  @apply absolute;
 }
-
-
-
 </style>
